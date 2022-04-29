85214ccb7bdd800f8fe9650c5c1f82426b6e67e9
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-04-29 21:45:32 +0300

    Dynamic navmesh added to obstacles

--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -8,6 +8,7 @@ public class Opponent : MonoBehaviour
     
     [SerializeField] GameObject finishLine;
     Transform finishLineTransform;
+    
     NavMeshAgent nav;
     
     void Start()
@@ -15,14 +16,15 @@ public class Opponent : MonoBehaviour
         finishLineTransform = finishLine.transform;
        
         nav = GetComponent<NavMeshAgent>();
-        nav.SetDestination(finishLineTransform.position);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
+        nav.SetDestination(finishLineTransform.position);
+
     }
   
  
